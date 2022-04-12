a7d10b18395167020ab58d66ffa7bc733cd263f0
fengpengfei111 <1479623700@qq.com>
2022-04-12 23:21:41 +0800

    push

---
+++ eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
